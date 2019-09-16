2f0b95c41e9713f267817b279fbb24a6c6b86ec1
Mike H <matroxmike@icloud.com>
2019-09-16 14:28:35 +1000

    removed example unicode.rs code

--- unicode.rs
+++
@@ -1,8 +0,0 @@
-extern crate unicode_xid;
-
-use unicode_xid::UnicodeXID;
-
-fn main() {
-    let ch = 'a';
-    println!("Is {} a valid start of an identifier? {}", ch, UnicodeXID::is_xid_start(ch));
-}
